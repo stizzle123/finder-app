2027cfd0db1604294fbc11ec94700170a7ba5d47
stizzle123 <arifayanidowu2@yahoo.com>
2019-08-19 22:06:45 +0100

    Init commit

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class UserItem extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Useritem</h2>
+      </div>
+    );
+  }
+}
+
+export default UserItem;
